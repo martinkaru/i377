8bb5adf36a2f2318f00add8631486056a20be2b0
Martin Karu <martinkaru@gmail.com>
2013-09-30 00:25:00 +0300

    Counter

--- src/SessionCount.java
+++ src/SessionCount.java
@@ -4,15 +4,31 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSessionEvent;
+import javax.servlet.http.HttpSessionListener;
 
-/**
- * Servlet implementation class SessionCount
- */
-public class SessionCount extends HttpServlet {
+public class SessionCount extends HttpServlet implements HttpSessionListener {
 	private static final long serialVersionUID = 1L;
+	private static int activeSessionCount = 0;
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
-		response.getWriter().printf("Sessions count: %d", SessionCountListener.getSessionCount());
+		response.getWriter().printf("Session count: %d", getSessionCount());
+	}
+
+	@Override
+	public void sessionCreated(HttpSessionEvent event) {
+		activeSessionCount++;
+	}
+
+	@Override
+	public void sessionDestroyed(HttpSessionEvent event) {
+		if (activeSessionCount > 0) {
+			activeSessionCount--;
+		}
+	}
+
+	public static int getSessionCount() {
+		return activeSessionCount;
 	}
 }
